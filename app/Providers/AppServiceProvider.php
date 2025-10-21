324cad4a95975009e6e09c0ab99160b61ca5f724
docteurZed <166328521+docteurZed@users.noreply.github.com>
2025-10-21 16:36:15 +0000

    ...

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\Candidat;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -22,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Schema::defaultStringLength(131);
+        
         View::composer(['candidate.*'], function ($view) {
             if (Auth::check()) {
                 $profil = Candidat::where('user_id', Auth::id())->first()->photo;
