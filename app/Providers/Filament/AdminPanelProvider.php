95d432a621fc19f2705208c562f8224c346ade6b
docteurZed <166328521+docteurZed@users.noreply.github.com>
2025-10-21 19:21:24 +0000

    ...

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -61,7 +61,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-                Cei::class,
+                // Cei::class,
             ])
             ->spa();
     }
