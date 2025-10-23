923b490a3066888488d5a7589fc06bc35833502d
docteurZed <166328521+docteurZed@users.noreply.github.com>
2025-10-23 09:06:42 +0000

    ...........................

--- resources/views/public/candidate-detail.blade.php
+++ resources/views/public/candidate-detail.blade.php
@@ -7,8 +7,21 @@
     <section class="container mx-auto max-w-5xl p-6 md:grid grid-cols-3 gap-8">
         {{-- En-tête du candidat --}}
         <div class="flex flex-col items-center text-center">
+            @php
+                $photo = '';
+
+                if ($candidat->photo) {
+                    $extension = pathinfo($candidat->photo, PATHINFO_EXTENSION);
+
+                    if ($extension == 'jpg') {
+                        $photo = 'candidatFile/photo-' . $candidat->user->student_number . '.jpg';
+                    } elseif ($extension == 'png') {
+                        $photo = 'candidatFile/photo-' . $candidat->user->student_number . '.png';
+                    }
+                }
+            @endphp
             <img class="w-36 h-36 rounded-full object-cover border-4 border-green-600 shadow-lg mb-4"
-                src="{{ $candidat->photo ? Storage::url($candidat->photo) : asset('img/profil.jpg') }}" alt="Candidat">
+                src="{{ $candidat->photo ? $photo : asset('img/profil.jpg') }}" alt="Candidat">
 
             <h1 class="text-2xl font-extrabold text-white mb-2">{{ $candidat->user->name }}</h1>
             @php
@@ -104,8 +117,7 @@
 
             @if ($candidat->affiche)
                 <div class="rounded-2xl shadow-md p-4 mt-8">
-                    <img src="{{ Storage::url($candidat->affiche) }}" alt="Affiche de campagne"
-                        class="rounded-xl shadow">
+                    <img src="{{ Storage::url($candidat->affiche) }}" alt="Affiche de campagne" class="rounded-xl shadow">
                 </div>
             @endif
 
